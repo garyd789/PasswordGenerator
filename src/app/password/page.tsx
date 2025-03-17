481d8207abae4a6f65b87b8ecfc2879ff42ae9c6
Gary <garyd9009@gmail.com>
2025-03-17 17:02:38 -0400

    fix: removed red border

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -4,7 +4,7 @@ import GeneratePasswordButton from "../components/button";
 export default function PasswordPage(){
 
     return (
-        <div className="border border-red-500 bg-gradient-to-b from-[#020024] to-[#090979] text-center flex flex-col h-screen items-center justify-center"> 
+        <div className="bg-gradient-to-b from-[#020024] to-[#090979] text-center flex flex-col h-screen items-center justify-center"> 
             <h1 className="text-4xl pb-20 text-white">
             Password Page 
             </h1>
